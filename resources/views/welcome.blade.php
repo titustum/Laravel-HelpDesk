1232aa4e1c505aa08987dc62a22ab58d19a06f08
titustum <tituskiptanuitum@gmail.com>
2024-06-24 00:39:11 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+
+    <div class="min-h-screen grid grid-cols-2">
+
+        <div class="flex flex-col justify-center">
+            <h1 class="text-center font-bold text-4xl text-cyan-600 font-['Righteous']">Tumaini HelpDesk</h1>
+            <img src="{{ asset('images/helpdesk-illustrator.webp') }}" class="mx-auto">
+        </div>
+
+        <div class="flex flex-col justify-center p-6">
+
+        </div>
+
+    </div>
+
+</x-app-layout>
